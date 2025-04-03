03212bf8fcb8cbde3930c5a72588fef63bf515e6
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-04-02 19:06:30 -0600

    feat: up and down player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float upForce;
+    Rigidbody2D rb;
+    private float yInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(0f, yInput * upForce));
+    }
+}
