0d04de27abaf86d7ab2b48e0bd0ce10b000d1a70
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-04-03 19:37:37 -0600

    feat: coroutine to create obstacles

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+    [SerializeField] private float spawnTime;
     [SerializeField] private GameObject obstaclePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnTime);
+        }
     }
 
     // Update is called once per frame
