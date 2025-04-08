16c1acda926d0cce20c9629661126454bae65e7e
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-04-07 18:42:15 -0600

    feat: menu game over and reload scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private GameObject gameOverPanel;
+    private bool isGameOver = false;
     private int score = 0;
 
     // Start is called before the first frame update
@@ -22,6 +25,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (isGameOver && Input.anyKeyDown)
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
     public void AddScore(int amount)
     {
         score += amount;
@@ -30,13 +46,29 @@ public class GameManager : MonoBehaviour
 
     public void StopGame()
     {
-        Spawner spawner = FindObjectOfType<Spawner>();
-        spawner.StopSpawning();
+        isGameOver = true;
+        StopSpawn();
+        StopScrolling();
+        ShowGameOverPanel();
+    }
 
+    private void ShowGameOverPanel()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    private void StopScrolling()
+    {
         Scroll[] scrollingObjects = FindObjectsOfType<Scroll>();
         foreach (Scroll scrollingObject in scrollingObjects)
         {
             scrollingObject.StopScrolling();
         }
+    }
+
+    private void StopSpawn()
+    {
+        Spawner spawner = FindObjectOfType<Spawner>();
+        spawner.StopSpawning();
     }
 }
